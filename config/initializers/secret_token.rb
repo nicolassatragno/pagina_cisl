2c686b12d1b1afd987fc48ff829ce7d0c026fd93
Nicolás Satragno <nsatragno@gnome.org>
2013-09-22 17:34:53 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaginaCisl::Application.config.secret_key_base = '52099dc2fc7d1a349c8b867616cd6d96b30bd2be3b36033258dbd95e2436c01ae8105ef15e880be767ee5f773b0ff1260af7ba69b49703614e2648c81f751e34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PaginaCisl::Application.config.session_store :cookie_store, key: '_pagina_cisl_session'

77119aec6aa62047a2a3750ce66bab279c7ce353
Nicolás Satragno <nsatragno@gnome.org>
2013-10-05 15:04:56 -0300

    Agregado guardado de formulario

--- app/controllers/ponencias_controller.rb
+++ app/controllers/ponencias_controller.rb
@@ -3,5 +3,18 @@ class PonenciasController < ApplicationController
   end
 
   def create
+    @ponencia = Ponencia.new(parametros)
+    @ponencia.save
+
+    redirect_to action: :show, id: @ponencia.id
   end
+
+  def show
+    @ponencia = Ponencia.find(params[:id])
+  end
+
+  private
+    def parametros
+      params.require(:ponencia).permit(:titulo, :expositor, :descripcion)
+    end
 end

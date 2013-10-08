4326da3100e58f5fc0d2d8d1d22c0a34cb24bcc9
Nicolás Satragno <nsatragno@gnome.org>
2013-10-07 22:56:43 -0300

    Agregados niveles

--- app/controllers/ponencias_controller.rb
+++ app/controllers/ponencias_controller.rb
@@ -17,6 +17,18 @@ class PonenciasController < ApplicationController
   def show
     @ponencia = Ponencia.find(params[:id])
   end
+  
+  def destroy
+    ponencia = Ponencia.find(params[:id])
+
+    if ponencia.destroy  
+      flash[:mensaje] = "La ponencia " + ponencia.titulo + " se ha borrado"
+    else
+      flash[:mensaje] = "No se pudo borrar la ponencia"
+    end
+      
+    redirect_to raiz_index_path
+  end
 
   private
     def parametros
